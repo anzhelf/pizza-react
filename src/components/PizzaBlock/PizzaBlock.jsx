8e0a36e3f59a478431e8f9b2329706c90a4f0dbf
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-08-14 15:27:46 +0300

    add card

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,21 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './PizzaBlock.scss';
-import Pizza from '../../images/pizza.svg';
 
-function PizzaBlock({ image, title, info }) {
+function PizzaBlock({ image, title, price, info, id }) {
+  const [type, setType] = React.useState(0);
+  const [size, setSize] = React.useState(0);
+  const [pizzaCount, setPizzaCount] = React.useState(0);
+
   return (
     <div className="pizza-block">
-      <img className="pizza-block__image" src={Pizza} />
-      <h3 className="pizza-block__title">Чизбургер-пицца</h3>
-      <div className="pizza-block__info"></div>
+      <Link className="pizza-block__link" key={id} to="#">
+        <img className="pizza-block__image" src={image} />
+        <h4 className="pizza-block__title">{title}</h4>
+      </Link>
+
+      <div className="pizza-block__options">
+        <ul className="pizza-block__list">
+          <li onClick={() => setSize(0)} className={`pizza-block__type ${size === 0 && 'active'}`}>
+            <p className="pizza-block__type-text">тонкое</p>
+          </li>
+          <li onClick={() => setSize(1)} className={`pizza-block__type ${size === 1 && 'active'}`}>
+            <p className="pizza-block__type-text">традиционное</p>
+          </li>
+        </ul>
+
+        <ul className="pizza-block__list">
+          <li onClick={() => setType(0)} className={`pizza-block__type ${type === 0 && 'active'}`}>
+            <p className="pizza-block__type-text">{`${26} см.`}</p>
+          </li>
+          <li onClick={() => setType(1)} className={`pizza-block__type ${type === 1 && 'active'}`}>
+            <p className="pizza-block__type-text">{`${30} см.`}</p>
+          </li>
+          <li onClick={() => setType(2)} className={`pizza-block__type ${type === 2 && 'active'}`}>
+            <p className="pizza-block__type-text">{`${40} см.`}</p>
+          </li>
+        </ul>
+      </div>
 
       <div className="pizza-block__button-block">
-        <span className="pizza-block__price">от 395 ₽</span>
+        <span className="pizza-block__price">от {price} ₽</span>
 
-        <button className="pizza-block__button">
-          <p className="pizza-block__add">+</p>
-          <p className="pizza-block__text">Добавить</p>
+        <button onClick={() => setPizzaCount(pizzaCount + 1)} className="pizza-block__button">
+          <span className="pizza-block__add">+</span>
+          <span className="pizza-block__text">Добавить</span>
           <div className="pizza-block__box">
-            <p className="pizza-block__item">2</p>
+            <span className="pizza-block__item">{pizzaCount}</span>
           </div>
         </button>
       </div>
