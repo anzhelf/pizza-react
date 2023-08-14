a874cfc097fc355cd06dd117bc2a5f6f8d7b3bcc
Anzhelika Shilyaeva <shilyaeva068@icloud.com>
2023-08-14 13:34:33 +0300

    add disign card

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,13 +1,24 @@
 import './PizzaBlock.scss';
 import Pizza from '../../images/pizza.svg';
 
-function PizzaBlock() {
+function PizzaBlock({ image, title, info }) {
   return (
     <div className="pizza-block">
       <img className="pizza-block__image" src={Pizza} />
       <h3 className="pizza-block__title">Чизбургер-пицца</h3>
       <div className="pizza-block__info"></div>
-      <div className="pizza-block__button-block"></div>
+
+      <div className="pizza-block__button-block">
+        <span className="pizza-block__price">от 395 ₽</span>
+
+        <button className="pizza-block__button">
+          <p className="pizza-block__add">+</p>
+          <p className="pizza-block__text">Добавить</p>
+          <div className="pizza-block__box">
+            <p className="pizza-block__item">2</p>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
