530565d47a0d6f1d2b6d7ad751e522ecf4b58dfa
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-08-29 08:51:20 +0300

    add pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,39 +1,52 @@
-import './Pagination.scss';
+import style from './Pagination.module.scss';
+import ReactPaginate from 'react-paginate';
 
-function Pagination() {
+console.log(style.root);
+
+const Pagination = () => {
   return (
-    <ul className="pagination">
-      <li className="pagination__element">
-        <a href="#" className="pagination__button">
-          {String.fromCharCode(60)}
-        </a>
-      </li>
+    // <ul className="pagination">
+    //   <li className="pagination__element">
+    //     <a href="#" className="pagination__button">
+    //       {String.fromCharCode(60)}
+    //     </a>
+    //   </li>
 
-      <li className="pagination__element active">
-        <a href="#" className="pagination__button">
-          1
-        </a>
-      </li>
+    //   <li className="pagination__element active">
+    //     <a href="#" className="pagination__button">
+    //       1
+    //     </a>
+    //   </li>
 
-      <li className="pagination__element">
-        <a href="#" className="pagination__button">
-          2
-        </a>
-      </li>
+    //   <li className="pagination__element">
+    //     <a href="#" className="pagination__button">
+    //       2
+    //     </a>
+    //   </li>
 
-      <li className="pagination__element">
-        <a href="#" className="pagination__button">
-          3
-        </a>
-      </li>
+    //   <li className="pagination__element">
+    //     <a href="#" className="pagination__button">
+    //       3
+    //     </a>
+    //   </li>
 
-      <li className="pagination__element">
-        <a href="#" className="pagination__button">
-          {String.fromCharCode(62)}
-        </a>
-      </li>
-    </ul>
+    //   <li className="pagination__element">
+    //     <a href="#" className="pagination__button">
+    //       {String.fromCharCode(62)}
+    //     </a>
+    //   </li>
+    // </ul>
+    <ReactPaginate
+      className={style.root}
+      breakLabel="..."
+      nextLabel=">"
+      previousLabel="<"
+      onPageChange={(e) => console.log(e)}
+      pageRangeDisplayed={8}
+      pageCount={3}
+      renderOnZeroPageCount={null}
+    />
   );
-}
+};
 
 export default Pagination;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import Sort from '../components/Sort/Sort';
 import Pagination from '../components/Pagination/Pagination';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
 import Skeleton from '../components/Skeleton/Skeleton';
-// import { URL } from '../constants/constants';
 
 function Home({ searchValue }) {
   const [items, setItems] = React.useState([]);
@@ -19,28 +18,22 @@ function Home({ searchValue }) {
   });
 
   const skelet = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
-  const pizzas = items
-    .filter((obj) => {
-      if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-        return true;
-      }
-      return false;
-    })
-    .map((obj, i) => <PizzaBlock {...obj} key={i} />);
+  const pizzas = items.map((obj, i) => <PizzaBlock {...obj} key={i} />);
 
   const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
   const sortBy = sortType.sortProperty.replace('-', '');
   const category = categoryId > 0 ? `category=${categoryId}` : '';
 
+  const search = searchValue ? `&search=${searchValue}` : '';
+
   React.useEffect(() => {
     setIsLoadig(true);
     fetch(
-      `https://64bae2425e0670a501d6b934.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://64bae2425e0670a501d6b934.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
-        console.log(json);
       })
       .catch((e) => console.error('error:', e))
       .finally(() => {
@@ -48,7 +41,7 @@ function Home({ searchValue }) {
       });
     //чтоб при переходе по ссылке делался скролл вверх
     window.scroll(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
 
   return (
     <>
@@ -65,7 +58,7 @@ function Home({ searchValue }) {
       <div className="pizza-list">{isLoading ? skelet : pizzas}</div>
 
       {/* <Error /> */}
-      {/* <Pagination /> */}
+      <Pagination />
     </>
   );
 }
